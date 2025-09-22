88c99daa84a14e2969c4825446b6272e39bb027a
Evgeniya <evgeniyapylypenko92@gmail.com>
2025-09-22 13:46:41 +0300

    task4

---
+++ js/task-4.js
@@ -0,0 +1,23 @@
+let sbmtFormElem = document.querySelector(".login-form");
+
+sbmtFormElem.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const sbmtForm = event.target;
+  const email = sbmtForm.elements.email.value.trim();
+  const pswrd = sbmtForm.elements.password.value.trim();
+  const user = {
+    email: "",
+    password: "",
+  };
+
+  if (email === "" || pswrd === "") {
+    alert("All form fields must be filled in");
+  } else {
+    user.email = email;
+    user.password = pswrd;
+    console.log(user);
+    sbmtForm.reset();
+  }
+}
