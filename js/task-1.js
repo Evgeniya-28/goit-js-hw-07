c22b4f77f09a656b400111222531d7eef469c80d
Evgeniya <evgeniyapylypenko92@gmail.com>
2025-09-22 13:39:01 +0300

    task1

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+// let categories = document.querySelector("#categories");
+let categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((category) => {
+  let categoryName = category.querySelector("h2").textContent;
+  let elementsNumb = category.querySelectorAll("li").length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${elementsNumb}`);
+});
