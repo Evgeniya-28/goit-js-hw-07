47c4a2b15206c5148522503a9501cb3e8889f62d
Evgeniya <evgeniyapylypenko92@gmail.com>
2025-09-22 13:47:51 +0300

    task5

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let btnChngColorElem = document.querySelector(".change-color");
+let colorElem = document.querySelector(".color");
+let bodyElem = document.querySelector("body");
+
+btnChngColorElem.addEventListener("click", () => {
+  let color = getRandomHexColor();
+  colorElem.innerHTML = color;
+  bodyElem.style.backgroundColor = color;
+});
